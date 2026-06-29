32bd941f5cc0b62ddd6e98879a62ea13e0055bed
Niellazzyy <danielmoralesquinaso@gmail.com>
2026-06-29 10:42:23 +0800

    calculator
    Implement a simple calculator that performs addition, subtraction, multiplication, and division.

--- calculator.c
+++ calculator.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
 
+int main() {
+
+    float num1, num2, result;
+    char op;
+
+    printf("===== SIMPLE CALCULATOR =====\n");
+
+    printf("Enter the first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter the second number: ");
+    scanf("%f", &num2);
+
+    if (op == '+') {
+
+        result = num1 + num2;
+        printf("Result = %.2f", result);
+
+    } else if (op == '-') {
+
+        result = num1 - num2;
+        printf("Result = %.2f", result);
+
+    } else if (op == '*') {
+
+        result = num1 * num2;
+        printf("Result = %.2f", result);
+
+    } else if (op == '/') {
+
+        if (num2 != 0) {
+            result = num1 / num2;
+            printf("Result = %.2f", result);
+        } else {
+            printf("Error! Cannot divide by zero.");
+        }
+
+    } else {
+
+        printf("Invalid operator!");
+
+    }
+
+    return 0;
+}
