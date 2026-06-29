a28a0715169d7d777ebbedbc6f09770294b6c2fd
Niellazzyy <danielmoralesquinaso@gmail.com>
2026-06-29 10:34:25 +0800

    Add files via upload

--- hello.c
+++ hello.c
@@ -2,37 +2,33 @@
 
 int main (){
 
-    float num1, num2, result;
-    char op;
+    float prelim, midterms, finals, average;
 
-    printf("Enter the First Number:");
-    scanf("%f", &num1);
+    printf("Enter your Prelim Grade:");
+    scanf("%f", &prelim);
 
-    printf("Choose a Operator (+, -, *, /):");
-    scanf(" %c", &op);
+    printf("Enter your Midterms Grade:");
+    scanf("%f", &midterms);
 
-    printf("Enter the Second Number:");
-    scanf("%f", &num2);
+    printf("Enter your Final Grades:");
+    scanf("%f", &finals);
 
-    if(op == '+'){
-        result = num1 + num2;
-        printf("Result = %.2f\n", result);
-    } else if (op == '-'){
-        result = num1 - num2;
-        printf("Result = %.2f\n", result);
-    } else if (op == '*'){
-        result = num1 * num2;
-        printf("Result = %.2f\n", result);
-    } else if (op == '/'){
-        if (num2 != 0){
-        result = num1 / num2;
-        printf("Result = %.2f\n", result);
+    average = (prelim + midterms + finals) / 3;
+
+    printf("Prelim Grade = %.2f\n", prelim);
+    printf("Midterms Grade = %.2f\n", midterms);
+    printf("Final Grade = %.2f\n", finals); 
+    printf("Average = %.2f\n", average);
+
+    if(average >= 95){
+        printf("Status = With High Honors ");
+    } else if (average >= 90){
+        printf("Status = With Honors");
+    } else if (average >= 85){
+        printf("Status = Achievers");
     } else {
-        printf("Error! Cannot be Divided by 0");
-        } }else {
-            printf("Invalid Operator!");
-        }
-
+        printf("No Awards");
+    }
 
 
     return 0;
